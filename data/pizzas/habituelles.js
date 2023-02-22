4e5c26349c12f58aa6ed381b0271e46346992d86
a-votre-idee <95037920+a-votre-idee@users.noreply.github.com>
2023-02-22 10:11:39 +0100

    tarif fev 2023

--- data/pizzas/habituelles.js
+++ data/pizzas/habituelles.js
@@ -3,88 +3,88 @@ module.exports = [
     name: 'Margharita',
     description: 'Tomate, olives, mozzarella',
     prices: {
-      petite: 4,
-      moyenne: 6.0,
-      grande: 7.0
+      petite: 4.5,
+      moyenne: 7.0,
+      grande: 8.0
     }
   },
   {
     name: 'Romana',
     description: 'Tomate, jambon blanc, mozzarella',
     prices: {
-      petite: 4.5,
-      moyenne: 8,
-      grande: 10.0
+      petite: 5,
+      moyenne: 9,
+      grande: 11.0
     }
   },
   {
     name: 'Reine',
     description: 'Tomate, jambon blanc, champignons de Paris , olives, mozzarella',
     prices: {
-      petite: 5,
-      moyenne: 9,
-      grande: 11
+      petite: 5.5,
+      moyenne: 10,
+      grande: 12
     }
   },
   {
     name: 'Merguez',
     description: 'Crème fraîche, purée de poivrons, merguez, mozzarella',
     prices: {
-      petite: 6,
-      moyenne: 10,
-      grande: 12
+      petite: 6.5,
+      moyenne: 11,
+      grande: 13
     }
   },
   {
     name: 'Calzone (chausson)',
     description: 'Tomate, jambon blanc, champignons de Paris persillés, crème fraîche, œuf, mozzarella',
     prices: {
-      moyenne: 10,
-      grande: 12
+      moyenne: 11,
+      grande: 13
     }
   },
   {
     name: 'Carbonara',
     description: 'Crème fraîche, jambon blanc, lardons, œuf, mozzarella',
     prices: {
-      petite: 6,
-      moyenne: 10,
-      grande: 12
+      petite: 6.5,
+      moyenne: 11,
+      grande: 13
     }
   },
   {
     name: 'Kebab',
     description: 'Crème fraîche, viande kebab, mozzarella',
     prices: {
-      petite: 6,
-      moyenne: 10.5,
-      grande: 12.50
+      petite: 6.5,
+      moyenne: 11.5,
+      grande: 13.50
     }
   },
   {
     name: 'Texane',
     description: 'Tomate, viande hachée de boeuf, oignon, poivron, ventrêche, oeuf(s), persillade',
     prices: {
-      petite: 6,
-      moyenne: 11,
-      grande: 13
+      petite: 6.5,
+      moyenne: 12,
+      grande: 14
     }
   },
   {
     name: 'Paysanne',
     description: 'Tomate, champignons, lardons, oeuf, persillade, mozzarella',
     prices: {
-      moyenne: 10,
-      grande: 12
+      moyenne: 11,
+      grande: 13
     }
   },
   {
     name: 'Mixted',
     description: 'Crème fraîche, purée de poivrons, chorizo, merguez, mozzarella',
     prices: {
-      petite: 6,
-      moyenne: 10,
-      grande: 12
+      petite: 6.5,
+      moyenne: 11,
+      grande: 13
     }
   }
 ]
