dec0cccf92da5bbdc6e7469c351be22d196636bc
a-votre-idee <95037920+a-votre-idee@users.noreply.github.com>
2023-02-22 10:14:07 +0100

    tarifs fev 2023

--- data/pizzas/charcutieres.js
+++ data/pizzas/charcutieres.js
@@ -3,18 +3,18 @@ module.exports = [
     name: 'Mixed',
     description: 'Crème fraîche, purée de poivrons, chorizo, mozzarella',
     prices: {
-      petite: 6.0,
-      moyenne: 10.0,
-      grande: 12.0
+      petite: 6.5,
+      moyenne: 11.0,
+      grande: 13.0
     }
   },
   {
     name: 'Charcutière',
     description: 'Tomate ou crème fraîche, 3 charcuteries, mozzarella',
     prices: {
-      petite: 6,
-      moyenne: 10.0,
-      grande: 12
+      petite: 6.5,
+      moyenne: 11.0,
+      grande: 13
     }
   }
 ]
